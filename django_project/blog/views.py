ac7f971224c6157efda864254f367610b1ddec60
albertosdneto <albertosdneto@gmail.com>
2019-09-28 15:15:22 -0300

    Templates. Passing variables to templates. Templates inheritance

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
+
+posts_list = [
+    {
+        'author': 'Alberto',
+        'title': 'Blog Post 1',
+        'content': 'Conteudo do primeiro post',
+        'date_posted': 'September 28, 2019'
+    },
+
+    {
+        'author': 'Jack',
+        'title': 'Blog Post 2',
+        'content': 'Conteudo do SEGUNDO post',
+        'date_posted': 'September 28, 2019'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {'posts': posts_list}
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})

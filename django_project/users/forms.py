4cb2f387c658ddadd7737d0e0ce1a9a87aa01577
Thiago Rabelo <thiagorabelo1@gmail.com>
2019-10-05 12:44:37 -0300

    Mais sugestoes na app users

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,7 +1,11 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
 from .models import Profile
+
+# https://docs.djangoproject.com/en/2.2/topics/auth/customizing/#referencing-the-user-model
+User = get_user_model()  # pylint: disable=invalid-name
 
 
 class UserRegisterForm(UserCreationForm):
--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,18 +1,20 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 from PIL import Image
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # https://docs.djangoproject.com/en/2.2/topics/auth/customizing/#referencing-the-user-model
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
+    # Tente mantenha a mesma assinatura do método que você está sobrescrevendo.
+    # Altere a assinatura somente se for intencional.
     def save(self, force_insert=False, force_update=False, using=None,
-             update_fields=None):  # Mantenha a mesma assinatura do
-                                   # método que você está sobrescrevendo
+             update_fields=None):
 
         super().save(force_insert, force_update,
                      using, update_fields)
